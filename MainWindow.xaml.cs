5e7825e7b977fb3d0564950b86efc79cc0ea6e59
aYoungGameDev <71611000+aYoungGameDev@users.noreply.github.com>
2022-03-26 20:45:08 -0400

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            SetUpGame();
+        }
+        private void SetUpGame()
+        {
+            List<string> foodEmoji = new List<string>()
+            {
+                "🍕","🍕",
+                "🍔","🍔",
+                "🌮","🌮",
+                "🌯","🌯",
+                "🥙","🥙",
+                "🍲","🍲",
+                "🧇","🧇",
+                "🍦","🍦",
+            };
+            Random rand = new Random();
+            foreach (TextBlock textBlock in mainGrid.Children.OfType<TextBlock>())
+            {
+                int index = rand.Next(foodEmoji.Count);
+                string nextEmoji = foodEmoji[index];
+                textBlock.Text = nextEmoji;
+                foodEmoji.RemoveAt(index);
+            }
+        }
+    }
+}
